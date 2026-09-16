2e7ab26a2c455b767b9dca4d659a954a04223be9
wwwyayadembele46-lab <wwwyayadembele46@gmail.com>
2026-09-16 22:52:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -230,5 +230,4 @@ def class_photo(cid):
     return redirect(url_for("dashboard"))
 
 if __name__=="__main__":
-    init_db()
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
